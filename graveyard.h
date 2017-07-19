598ea2b2077ed5162fc8bb8a7fcbb8708370dea5
Chen <cruguang@gmail.com>
2017-07-19 17:13:09 -0400

    Update graveyard.h

--- graveyard.h
+++ graveyard.h
@@ -1,6 +1,6 @@
 #ifndef graveyard_h
 #define graveyard_h
-
+#include <boolean>
 #include <vector>
 #include "card.h"
 #include <string>
@@ -12,6 +12,8 @@ public:
     Graveyard();
     ~Graveyard();
     string getTop();     //gets the name of the card and deletes the pointer
+    bool isEmpty();
+    void addMinion();
 }
 
 
