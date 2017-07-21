e14237d63cae94c84be3e7e745ffddcb788e0ba4
RaymondWaterlooLi <liyuening316@gmail.com>
2017-07-20 21:50:15 -0400

    have a new branch

---
+++ try1.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "ascii_graphics.h"
+#include <vector>
+
+using namespace std;
+
+int main() {
+	card_template_t a = display_minion_no_ability("hey", 3, 5, 6);
+
+	//cout << "f";
+	// for (auto e : a) {
+	//  cout << e;
+	// }
+}
