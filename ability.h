7793eea051b8133a13792038718a7f0b062c2bcf
RaymondWaterlooLi <liyuening316@gmail.com>
2017-07-22 17:00:55 -0400

    update

--- ability.h
+++ ability.h
@@ -2,10 +2,12 @@
 #define ABA_H
 
 class Board;
+class Player;
 
 class ability {
 public:
-	void chooseAbility (Board &t, int choice);
+	void chooseAbility (Board &t, int choice, Player player, int target);
+
 };
 
 
